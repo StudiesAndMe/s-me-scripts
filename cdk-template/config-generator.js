057aad2d50676e9d0192367725b132d3034ee6f5
ddennis <dennis@fantastisk.dk>
2021-06-23 17:03:03 +0200

    feat: implement cdk generation

---
+++ cdk-template/config-generator.js
@@ -0,0 +1,118 @@
+'use strict'
+var inquirer = require('inquirer')
+
+//const chalkPipe = require('chalk-pipe')
+
+/*{
+	"CdkTemplateStackId": "tester-stack",
+	"PROJECT_NAME": "tester-",
+	"ENV_TYPE": "staging",
+	"FRONTEND_BUILD_FOLDER": "../build",
+	"HOSTED_DOMAIN_NAME": "studies-and-me.com",
+	"env": { "account": "736365631927", "region": "eu-central-1" }
+}*/
+
+const CRA_BUILD_FOLDER = '../build'
+const GATSBY_BUILD_FOLDER = '../public'
+
+const CRA_OPTION = 'create-react-app'
+const GATSBY_OPTION = 'gatsby'
+
+module.exports = function (callback) {
+  startGenerator().then((result) => {
+    callback(null, result)
+  })
+}
+
+let projectName = ''
+
+function startGenerator() {
+  return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'PROJECT_NAME',
+        message: 'What is the project name:',
+        validate(s) {
+          //@console.log(' tester > s = ', s)
+
+          if (s.indexOf(' ') !== -1) {
+            return 'Spaces is not valid'
+          }
+
+          projectName = s
+          return s.length > 2 ? true : 'you need to write proper name'
+        },
+      },
+
+      {
+        type: 'input',
+        name: 'CdkTemplateStackId',
+        message: 'Stack Id:',
+        validate(s) {
+          return s.length > 4 ? true : 'you need to write proper stack id'
+        },
+        default() {
+          return projectName + '-stack'
+        },
+      },
+
+      {
+        type: 'list',
+        name: 'FRONTEND_BUILD_FOLDER',
+        message: 'Frontend project type',
+        choices: [CRA_OPTION, GATSBY_OPTION],
+      },
+
+      {
+        type: 'input',
+        name: 'HOSTED_DOMAIN_NAME',
+        message: 'domain',
+        default() {
+          return 'studies-and-me.com'
+        },
+      },
+
+      {
+        type: 'input',
+        name: 'HOSTED_DOMAIN_NAME',
+        message: 'domain',
+        default() {
+          return 'studies-and-me.com'
+        },
+      },
+
+      {
+        type: 'input',
+        name: 'account',
+        message: 'domain',
+        default() {
+          return '736365631927'
+        },
+      },
+
+      {
+        type: 'input',
+        name: 'region',
+        message: 'AWS region',
+        default() {
+          return 'eu-central-1'
+        },
+      },
+    ])
+    .then((answers) => {
+      //console.log(JSON.stringify(answers, null, '  '))
+
+      const buildFolder = answers.FRONTEND_BUILD_FOLDER === CRA_OPTION ? CRA_BUILD_FOLDER : GATSBY_BUILD_FOLDER
+
+      const obj = {
+        PROJECT_NAME: answers.PROJECT_NAME,
+        CdkTemplateStackId: answers.CdkTemplateStackId,
+        FRONTEND_BUILD_FOLDER: buildFolder,
+        HOSTED_DOMAIN_NAME: answers.HOSTED_DOMAIN_NAME,
+        env: { account: answers.account, region: answers.region },
+      }
+      console.log(JSON.stringify(obj, null, '  '))
+      return obj
+    })
+}
---
+++ cdk-template/setup.js
@@ -0,0 +1,92 @@
+/**
+ * Created by @author @ddennis - ddennis.dk aka fantastisk.dk/works aka meresukker.dk on 02/06/2021.
+ */
+
+const path = require('path')
+const fs = require('fs')
+const admZip = require('adm-zip')
+const configGenerator = require('./config-generator')
+
+const templateGitUrl = 'https://github.com/StudiesAndMe/frontend-stack-cdk-template.git'
+const gitDownloadLink = 'https://github.com/StudiesAndMe/frontend-stack-cdk-template/archive/refs/heads/main.zip'
+const dest = 'myfile.zip'
+const OUTPUT_FOLDER = './infrastructure'
+
+const request = require('superagent')
+
+//"https://github.com/StudiesAndMe/frontend-stack-cdk-template/archive/refs/heads/main.zip"
+
+module.exports = function setup(env) {
+  configGenerator((err, result) => {
+    console.log(' setup > result = ', result)
+
+    const p = './cdk-config.json'
+    fs.writeFile(p, JSON.stringify(result, null, 2), (err) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      console.log(' setup > config file written = ')
+      downloadUnzip(gitDownloadLink, dest, (err, result) => {
+        console.log(' setup > err, result = ', err, result)
+      })
+    })
+  })
+}
+
+function downloadUnzip(urlToZipFile, dest, cb) {
+  console.log(' CDK > download ZIP file ')
+  request
+    .get(urlToZipFile)
+    .on('error', function (error) {
+      console.log(error)
+      cb(error)
+    })
+    .pipe(fs.createWriteStream(dest))
+    .on('finish', function () {
+      // add code below to here
+      console.log('zip file downloaded')
+      var zip = new admZip(dest)
+      console.log('unzip')
+      zip.extractEntryTo('frontend-stack-cdk-template-main/', `./`, true, true)
+      //zip.extractAllTo('./tester', true)
+
+      const exist = fs.existsSync(OUTPUT_FOLDER)
+      if (exist) {
+        deleteDir(OUTPUT_FOLDER, (err, result) => {
+          rename(cb)
+        })
+      } else {
+        rename(cb)
+      }
+    })
+}
+
+function rename(cb) {
+  fs.rename('./frontend-stack-cdk-template-main', OUTPUT_FOLDER, function (err) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('Successfully renamed the directory.')
+
+      fs.unlink('myfile.zip', (err) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        console.log('Deleted file zip file')
+        cb(null, true)
+        //file removed
+      })
+    }
+  })
+}
+
+function deleteDir(dir, callback) {
+  fs.rmdir(dir, { recursive: true }, (err, res) => {
+    if (err) {
+      console.error(err)
+    }
+    callback(err, res)
+  })
+}
